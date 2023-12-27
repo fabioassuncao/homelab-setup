410ef40692966d8e52341585d47d26d4dcb4873c
Fábio Assunção <fabio23gt@gmail.com>
2023-12-26 23:43:13 -0300

    Small fixes

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ usage() {
     cat 1>&2 <<HERE
 Script for initial configurations of Docker, Docker Swarm and CapRover.
 USAGE:
-    wget -qO- https://fabioassuncao.com/gh/homelab-setup/install.sh | bash -s -- [OPTIONS]
+    curl -sSL https://fabioassuncao.com/gh/homelab-setup/install.sh | bash -s -- [OPTIONS]
 
 OPTIONS:
 -h|--help                   Print help
